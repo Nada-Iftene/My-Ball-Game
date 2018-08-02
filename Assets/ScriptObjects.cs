89b18800b8c2209be133eb3ca51aa94aced56421
Nada-Iftene <32202745+Nada-Iftene@users.noreply.github.com>
2018-08-02 14:44:02 +0200

    Add files via upload

---
+++ Assets/ScriptObjects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptObjects : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(100, 0, 100) * Time.deltaTime);
+
+
+    }
+}
---
+++ Assets/Scripts/Capp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capp : MonoBehaviour {
+    void Update()
+    {
+        transform.Rotate(new Vector3(100, 100, 100) * Time.deltaTime);
+
+
+
+    }
+}
